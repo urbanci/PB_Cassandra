ab878854126551997804f572d5025e036b1b4a70
urban <urban.kos93@gmail.com>
2018-01-12 15:06:42 +0100

    Hey testiram

--- src/main/java/cassandra/Main.java
+++ src/main/java/cassandra/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args){
         PolniBazo polniBazo = new PolniBazo();
         System.out.println(polniBazo.polniBazo(20,10, 5));
-        System.out.println("hello");
     }
 
 }
